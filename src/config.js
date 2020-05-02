8722fedd38e7bfd373f876ba3b81ec60e9d2b7c0
msaaddev <mrsaadirfan@gmail.com>
2020-05-02 18:09:04 +0500

    👌 IMPROVE: config.js

--- src/config.js
+++ src/config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Saad Irfan | Software Engineer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Saad leads one of the Google Student Communities — Developer Student Clubs (DSC) — at his varsity. By night, he dabbles with React.js, Gatsby.js, and Node.js. ',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+    'Saad Irfan, Saad, Irfan, msaaddev, software engineer, front-end engineer, web developer, javascript',
   siteUrl: 'https://brittanychiang.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Saad Irfan',
+  location: 'Toronto, Canada',
+  email: 'mrsaadirfan@gmail.com',
+  github: 'https://github.com/msaaddev',
+  twitterHandle: '@msaaddev',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/msaaddev',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/msaaddev',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/msaaddev',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/msaaddev',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/msaaddev',
     },
   ],
 
